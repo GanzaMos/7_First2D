65a43ef4cce6d1a1f155798bc8bfadf9be3b0eaf
GanzaMos <moskalik.ganza@gmail.com>
2020-08-05 12:47:24 +0300

    1 start of the project

---
+++ Assets/Scenes/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] private Text storyText;
+    
+    void Start()
+    {
+        storyText.text = ("First programm text");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/State.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Scenes
+{
+    [CreateAssetMenu(menuName = "StateScript")]
+    public class State : ScriptableObject
+    {
+        [TextArea(10,14)][SerializeField] private string storyText;
+    }
+}
